d99da27244663deb14ec3363304c3892a2101440
Bruce <bruce@redravencomputing.com>
2025-06-16 00:31:50 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/redravencomputing/magic_8_ball_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.redravencomputing.magic_8_ball_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: BallPage()));
+
+class BallPage extends StatelessWidget {
+  const BallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade900,
+          title: Text('Ask Me Anything', style: TextStyle(color: Colors.white)),
+        ),
+        body: Ball(),
+      ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          print("I got clicked");
+        },
+        child: Image.asset('images/ball1.png'),
+      ),
+    );
+  }
+}
